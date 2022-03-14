0dac8972c386c5cb9fab475590d31533965e480b
Brian Donnoe <dunow33@hotmail.com>
2022-03-14 17:44:04 -0400

    needs some work

---
+++ app/src/main/java/com/example/roomdbinjava/UserListAdapter.java
@@ -0,0 +1,60 @@
+package com.example.roomdbinjava;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.roomdbinjava.db.User;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.MyViewHolder> {
+
+    private Context context;
+    private List<User> userList;
+    public UserListAdapter(Context context){
+        this.context = context;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public UserListAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.recycler_row, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserListAdapter.MyViewHolder holder, int position) {
+        holder.tvFirstName.setText(this.userList.get(position).firstName);
+        holder.tvLastName.setText(this.userList.get(position).lastName);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvFirstName;
+        TextView tvLastName;
+
+        public MyViewHolder(View view) {
+            super(view);
+            TextView tvFirstName = view.findViewById(R.id.tvFirstName);
+            TextView tvLastName = view.findViewById(R.id.tvLastName);
+        }
+    }
+}
